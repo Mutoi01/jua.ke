be8d6ee81a3c3355894e22fb52f4b422f8730ddb
Mutoi01 <pngando9@gmail.com>
2022-06-24 06:52:16 +0300

    add image folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = "http://openlibrary.org/search.json?q="
 function getbooks(){
   document.querySelector('.output').innerHTML = "";
-  fetch(url+document.getElementById("input").value
+  fetch(url+document.getElementById("input").value)
 
 
 
